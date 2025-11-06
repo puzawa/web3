51c4f45eb635e2fc2eb1a055dca4834f09261d7a
vazupa <vazupa@DESKTOP-BJC1J91>
2025-11-07 01:56:03 +0300

    jsf test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,18 +10,33 @@ repositories {
 dependencies {
     implementation("org.glassfish:jakarta.faces:4.0.1")
 
-    implementation("jakarta.enterprise:jakarta.enterprise.cdi-api:4.0.1")
-    implementation("jakarta.validation:jakarta.validation-api:3.0.2")
-    implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
-    implementation("jakarta.servlet:jakarta.servlet-api:6.0.0")
+    providedCompile("jakarta.servlet:jakarta.servlet-api:6.0.0")
+
+    providedCompile("jakarta.enterprise:jakarta.enterprise.cdi-api:4.0.1")
+    providedCompile("jakarta.inject:jakarta.inject-api:2.0.1")
+
+    implementation("org.primefaces:primefaces:13.0.1:jakarta")
 
     implementation("org.hibernate.orm:hibernate-core:6.3.0.Final")
 
-    implementation("org.postgresql:postgresql:42.7.4")
+    implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
 
-    implementation("com.google.guava:guava:32.1.3-jre")
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("org.postgresql:postgresql:42.7.7")
 
+    implementation("org.slf4j:slf4j-api:2.0.9")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.16.2")
+
+    providedCompile("jakarta.ws.rs:jakarta.ws.rs-api:3.1.0")
+
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    sourceCompatibility = "17"
+    targetCompatibility = "17"
 }
 
 tasks.war {
